769ee98de28515838487afce042499151b23311e
scott <scott@Scotts-MacBook-Pro.local>
2022-11-11 15:36:50 -0700

    update

--- tests/balance_sheet_test.py
+++ tests/balance_sheet_test.py
@@ -5,9 +5,11 @@ from datetime import date
 
 class BalanceSheetTest(unittest.TestCase):
     """
-    python -m unittest tests/balance_sheet_test.py
-    python -m unittest tests/balance_sheet_test.py test.BalanceIncome.test_apply
-
+    python -m unittest tests
+    python -m unittest tests.balance_sheet_test
+    python -m unittest tests.balance_sheet_test.BalanceSheetTest
+    python -m unittest tests.balance_sheet_test.BalanceSheetTest.test_roe
+    
     Args:
         unittest (_type_): _description_
     """
@@ -17,9 +19,9 @@ class BalanceSheetTest(unittest.TestCase):
     def setUp(self):
         self.__wmt = BalanceIncome("WMT",date.today())
 
-    def test_apply(self):
+    def test_roe(self):
         ret = self.__wmt.get_return_on_equity()
         print(ret)  
 
 if __name__=="__main__":
-    unittest.main()    
+    unittest.main() 
